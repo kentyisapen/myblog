714b088d28d8fdac25c2a1de11c7714acabbc185
kentyisapen <kenkenpoco@gmail.com>
2021-07-12 02:21:41 +0000

    add normal user

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ User.create!(name:  "kentyisapen",
              password_confirmation: "Kent3141592",
              admin: true,
              )
+User.create(name:  "kentyisapen2",
+             password:              "Kent3141592",
+             password_confirmation: "Kent3141592",
+             admin: false,
+             )
