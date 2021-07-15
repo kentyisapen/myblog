797fc7fa1055745a4e98d8221110675b86bec028
kentyisapen <kenkenpoco@gmail.com>
2021-07-15 08:19:02 +0000

    simple layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,26 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+	var headNav = $(".header2");
+	//scrollだけだと読み込み時困るのでloadも追加
+	$(window).on('load scroll', function () {
+		//現在の位置が500px以上かつ、クラスfixedが付与されていない時
+		if($(this).scrollTop() > 200 && headNav.hasClass('fixed') == false) {
+		    headNav.css({"display": 'inline-block'});
+			//headerの高さ分上に設定
+			headNav.css({"top": '-100px'});
+			//クラスfixedを付与
+			headNav.addClass('fixed');
+			//位置を0に設定し、アニメーションのスピードを指定
+			headNav.animate({"top": 10},600);
+			headNav.css({"right": '10px'});
+		}
+		//現在の位置が300px以下かつ、クラスfixedが付与されている時にfixedを外す
+		else if($(this).scrollTop() < 300 && headNav.hasClass('fixed') == true){
+		    headNav.css({"display": 'none'});
+			headNav.removeClass('fixed');
+		}
+	});
+});
